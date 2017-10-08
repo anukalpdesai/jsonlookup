a098cb816d96fad0de6f5cb161f6808d0c07b975
anukalpdesai <anukalp.desai@gmail.com>
2017-10-09 00:05:57 +0530

    added .egg/* to gitignore and removed quotes from requirements.txt'

--- jsonlookup/setuplogging.py
+++
@@ -1,8 +0,0 @@
-""" This module sets up basic logging for jsonlookup """
-import logging
-
-
-def setup_logging(level=logging.DEBUG):
-    format_string = '%(name)s:(%levelno)s:%(filename)s:%(funcName)s%(lineno)d'
-    logging.basicConfig(level=level,
-                        format=format_string)
