d2ab50fc3cba4d7b66362ffbf3d7b39de156a7f0
anukalpdesai <anukalp.desai@gmail.com>
2017-10-09 01:17:49 +0530

    added doc string to main function in cli

--- jsonlookup/cli.py
+++ jsonlookup/cli.py
@@ -2,8 +2,12 @@
 import logging
 from .setup_logging import setup_logging
 
+import click
 
+
+@click.command()
 def main():
+    """ Lookup and correlate JSON key, value pairs"""
     setup_logging()
     logging.info('being')
 
