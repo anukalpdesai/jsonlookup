2ade5e7712d58da65cc2ed347ed6c8b25b53fd72
anukalpdesai <anukalp.desai@gmail.com>
2017-10-09 14:44:46 +0530

    added click to make jsonlookup command

--- jsonlookup/cli.py
+++ jsonlookup/cli.py
@@ -1,4 +1,5 @@
-""" Commandline Interface """
+"""Commandline Interface"""
+
 import logging
 from .setup_logging import setup_logging
 
@@ -6,10 +7,22 @@ import click
 
 
 @click.command()
-def main():
-    """ Lookup and correlate JSON key, value pairs"""
+@click.argument('src_json', type=click.Path(allow_dash=True), required=False)
+@click.argument('ref_json', type=click.Path(allow_dash=True), required=False)
+@click.option('-i', '--id', type=click.STRING,
+              help='Take value of this key from src_json')
+@click.option('-r', '--ref',
+              help='Reference location in ref_json to perform lookup')
+@click.option('-k', '--key',
+              help='Lookup the value of this key in ref_json')
+@click.option('-v', '--value',
+              help='If value of --id and --key matches, insert this key,'
+              ' value pair from ref_json into src_json')
+def main(**kwds):
+    """ Correlate values of --id in src_json and --key in ref_json
+        and on positive match insert --value into src_json"""
     setup_logging()
-    logging.info('being')
+    logging.debug(kwds)
 
 
 if __name__ == '__main__':
